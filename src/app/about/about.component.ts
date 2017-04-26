db547a9d670a40652c9b4c759904e6235a911182
Sam Kirsch <samdkirsch2@gmail.com>
2017-04-25 23:23:17 -0400

    add skeleton format and basic filler text to about section

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Image } from '../image.model';
+import { ImageService } from '../image.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
+  providers: [ImageService]
 })
 export class AboutComponent implements OnInit {
+  public images: Image[];
 
-  constructor() { }
+  constructor(private imageService: ImageService) { }
 
   ngOnInit() {
+    this.images = this.imageService.getImages();
   }
 
 }
